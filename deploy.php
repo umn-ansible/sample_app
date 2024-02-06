4c79b166f618316efc1bfec13ab34d5787903fd7
Colin McFadden <colin@hedge.video>
2024-02-06 14:58:51 -0600

    add deployer

---
+++ deploy.php
@@ -0,0 +1,24 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/umn-ansible/sample_app.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('dev')
+    ->set('remote_user', 'latis_deploy')
+    ->set('deploy_path', '/var/www/html/')
+    ->set('hostname', 'cla-mycareermanagment-dev-02.oit.umn.edu');
+
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
